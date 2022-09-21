bc31e69a8d2a5056ad2d861ff3cb877571259de1
Moringa School <elvis.rinkei@moringaschool.com>
2022-09-21 09:12:14 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,39 @@
+function add() {
+    return
+    }
+    function subtract(){
+    
+    }
+    function multiply(){
+    
+    }
+    function divide(){
+    
+    }
+function add(a, b){
+    return a + b
+}
+function subtract(a, b){
+    return a - b
+}
+function multiply(a,b){
+    return a / b
+}
+function multiply(a ,b){
+    return a * b
+}
+function divide(a , b){
+    return a / b
+}
+function increment(n){
+    return ++n
+}
+function decrement(n){
+return --n
+}
+function makeInt(String){
+    return parseInt(String,10)
+}
+function preserveDecimal(String){
+    return parseFloat(String)
+}
